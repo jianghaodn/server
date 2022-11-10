2b5f3ec56774603f004203416e4f889abf3cefff
jianghaodn <2690981804@qq.com>
2022-11-10 08:37:37 +0800

    test

--- src/main/java/com/project/demo/Application.java
+++ src/main/java/com/project/demo/Application.java
@@ -6,10 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
+ *
  */
 @SpringBootApplication
 @EnableJpaRepositories
-// @MapperScan("com.project.demo.dao")
+@MapperScan("com.project.demo.dao")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
